47122e6184d6ebe148d86eda5c568f9d803941e0
Joshua Clark <clarkis117@live.com>
2017-03-08 07:27:44 -0500

    vs2017 migration continued, and added projects for cortana voice service

---
+++ ClarkNet.Lib/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace ClarkNet.Lib
+{
+    public class Class1
+    {
+		public Task voi()
+		{
+			return Task.FromResult<object>(null);
+		}
+
+		public (int a, int b) Ver()
+		{
+			return (5,5);
+		}
+    }
+}
